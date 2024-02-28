d04c02aa698d5f1a8eb48678ab9c035c5a3185de
matheusfonte <fontematheus454@gmail.com>
2024-02-28 16:50:23 -0300

    feat: add signin page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "../components/Body";
 import HomeScreen from "../screens/HomeScreen";
 import SignUpPage from "../screens/SignUpPage";
+import SignInPage from "../screens/SignInPage";
  
 export default function RouterRoot() {
   return (
@@ -10,6 +11,8 @@ export default function RouterRoot() {
         <Route element={<Body />} path="/">
           <Route element={<HomeScreen />} path="/" />
           <Route element={<SignUpPage />} path="/signup" />
+          <Route element={<SignInPage
+           />} path="/signin" />
         </Route>
       </Routes>
     </BrowserRouter>
