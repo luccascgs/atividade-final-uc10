f12690b68d400ffa6302a8ab8b1499100622dd25
Luccas <luccascorreia.08@hotmail.com>
2024-02-28 14:53:41 -0300

    feat: add cards

--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -3,38 +3,77 @@ import { colors } from "../../variables";
 
 export const NavBar = styled.nav`
   display: flex;
+  position: fixed;
   flex-direction: column;
   justify-content: space-between;
-  align-items: center;
-  padding: 1rem 0;
   width: 350px;
-  height: 70vh;
+  height: 100vh;
   background-color: ${colors.lightGray};
   font-size: 1.1rem;
+  -webkit-box-shadow: 5px 0px 26px 0px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 5px 0px 26px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 5px 0px 26px 0px rgba(0, 0, 0, 0.2);
 `;
 export const SideButton = styled.div`
   display: flex;
   align-items: center;
-  margin: 0.5rem 0;
+  margin: 0.2rem 0;
   padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  width: 300px;
+  font-weight: 600;
   background-color: ${colors.white};
   * + * {
     padding-left: 1rem;
   }
+  &:hover {
+    color: white;
+    background-color: gray;
+    transition: all 0.2s;
+  }
 `;
-export const CreateButton = styled.button`
+export const CreateButton = styled.div`
   display: flex;
   justify-content: center;
   padding: 0.5rem 1rem;
-  width: 332px;
   background-color: ${colors.orange};
   color: ${colors.white};
-  font-size: 1.1rem;
-  border: solid 2px ${colors.darkOrange};
-  border-radius: 0.5rem;
+  font-weight: 600;
+  &:hover {
+    background-color: ${colors.darkOrange};
+    transition: all 0.2s;
+  }
 `;
 export const User = styled.div`
   display: flex;
+  padding: 0.5rem 1rem;
+  :nth-child(1) {
+    width: 1.2rem;
+    background-color: gray;
+    border: gray solid 4px;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+`;
+
+export const Content = styled.div`
+  margin-left: 350px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 3rem 2rem;
+  width: ${window.innerWidth - 350}px;
+`;
+
+export const Cards = styled.div``;
+
+export const CardsRow = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  h1 {
+    margin: 0;
+    margin-top: 2rem;
+  }
 `;
